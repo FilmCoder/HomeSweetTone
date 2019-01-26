4b52245fb2415891a4b069a736e52089d342b9a8
= <dominic.napoleon@gmail.com>
2019-01-26 01:06:46 -0500

    simple example white sphere can walk into and out of the screen, with walking animation going up and down, controlled by space bar

---
+++ Symphony/Assets/Scripts/Testing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // toggle inside/outside animation state
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetBool("IsInside", !animator.GetBool("IsInside"));
+            print("space key was pressed");
+        }
+    }
+}
---
+++ Symphony/Assets/Scripts/WalkingAnimationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingAnimationManager : MonoBehaviour
+{
+    private Animator animator;
+    private Animator parent_animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject parent = transform.parent.gameObject;
+        parent_animator = parent.GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // parent animator deals with setting the position of the character,
+        // and this objects animator deals with the "bounce" in the walk
+        if(parent_animator.IsInTransition(0))
+        {
+            Debug.Log("WALKING");
+            animator.SetBool("IsWalking", true);
+        } else
+        {
+            Debug.Log("NOT WALKING");
+            animator.SetBool("IsWalking", false);
+        }
+    }
+}
