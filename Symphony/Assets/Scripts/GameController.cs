2160b0231631397d0b3e5941c37e653ced6ac176
Tommy Lee <tommy.lee.says@gmail.com>
2019-01-27 02:18:05 -0500

    Modified bubble placements in scene. Added placeholder GameController script.

---
+++ Symphony/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] dialoguePanels;
+    public GameObject[] dialogueTexts;
+
+    private bool[] isCharacterInScene;
+
+    public enum CHARACTER {
+        GIRL = 0,
+        CAT = 1,
+        LADY = 2,
+        MAN = 3,
+        PLAYER = 5
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
