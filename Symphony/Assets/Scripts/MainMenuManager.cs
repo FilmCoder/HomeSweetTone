a5fcae9ea25584e57878a1bd33607c93b9e43c64
= <dominic.napoleon@gmail.com>
2019-01-27 06:59:33 -0500

    added credits, about page (but empty still), and a main menu

---
+++ Symphony/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Button startButton, creditsButton;
+
+    public void Start()
+    {
+        startButton.onClick.AddListener(startGame);
+        creditsButton.onClick.AddListener(rollCredits);
+    }
+
+    public void rollCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
